1aaed5ef9af9462097c46d80665dfc165b86fc18
Albert Llop <mrsimo@gmail.com>
2011-04-12 19:52:12 +0200

    Extra tests for key, some I don't know how to test

--- test/key_test.rb
+++ test/key_test.rb
@@ -2,6 +2,21 @@
 require 'test_helper'
 
 describe GPGME::Key do
+
+  it "has certain attributes" do
+    key = GPGME::Key.find(:secret).first
+    [:keylist_mode, :protocol, :owner_trust, :issuer_serial,
+      :issuer_name, :chain_id, :subkeys, :uids].each do |attrib|
+      assert key.respond_to?(attrib), "Key doesn't respond to #{attrib}"
+    end
+  end
+
+  it "won't allow the creation of GPGME::Key's without the C API" do
+    assert_raises NoMethodError do
+      GPGME::Key.new
+    end
+  end
+
   describe :find do
     it "should return all by default" do
       keys = GPGME::Key.find :secret
@@ -31,5 +46,44 @@ describe GPGME::Key do
       assert keys.empty?
     end
   end
+
+  # describe :trust do
+  #   it "returns :revoked if it is so"
+  #   it "returns :expired if it is expired"
+  #   it "returns :disabled if it is so"
+  #   it "returns :invalid if it is so"
+  #   it "returns nil otherwise"
+  # end
+
+  # describe :capability do
+  #   it "returns an array of possible capabilities"
+  # end
+
+  describe :usable_for? do
+    it "checks for the capabilities of the key and returns true if it matches all" do
+      key = GPGME::Key.find(:secret).first
+
+      key.stubs(:capability).returns([:encrypt, :sign])
+      assert key.usable_for?([])
+
+      key.stubs(:capability).returns([:encrypt, :sign])
+      assert key.usable_for?([:encrypt])
+
+      key.stubs(:capability).returns([:encrypt, :sign])
+      refute key.usable_for?([:certify])
+    end
+
+    it "returns false if the key is expired or revoked or disabled or disabled" do
+      key = GPGME::Key.find(:secret).first
+      key.stubs(:trust).returns(:revoked)
+      key.stubs(:capability).returns([:encrypt, :sign])
+      refute key.usable_for?([:encrypt])
+    end
+  end
+
+  # describe :secret? do
+  #   "returns true/false depending on the instance variable"
+  # end
+
 end
 
